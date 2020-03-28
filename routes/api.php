3b0902aa3e7c005f27f51999cc7fa3c1467245a0
MuAladdinIbrahim <muhammadalaa27@gmail.com>
2020-03-28 16:02:05 +0200

    sanctum-get token

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +23,23 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/posts', 'API\PostController@index');
-Route::get('/posts/{post}', 'API\PostController@show');
-Route::post('/posts','API\PostController@store');
+Route::get('/posts/{post}', 'API\PostController@show')->middleware('auth:sanctum');
+Route::post('/posts','API\PostController@store')->middleware('auth:sanctum');
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required'
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
